9ff3a520bb6c2c29742251974c328df406661a74
@hexlet/cli <support@hexlet.io>
2023-09-28 17:06:05 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -5,5 +5,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
+public class App {
 
+    public static boolean scrabble(String tiles, String word) {
+        if (word.length() > tiles.length()) {
+            return false;
+        }
+        char[] tilesArray = tiles.toLowerCase().toCharArray();
+        char[] wordArray = word.toLowerCase().toCharArray();
+        Arrays.sort(tilesArray);
+        Arrays.sort(wordArray);
+        int i = 0;
+        for (char letter : wordArray) {
+            while (i < tilesArray.length && tilesArray[i] < letter) {
+                i++;
+            }
+            if (i == tilesArray.length || tilesArray[i] > letter) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
 //END
