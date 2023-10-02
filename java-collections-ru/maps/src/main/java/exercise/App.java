c611803e9639aa45912198d9c34c355adff09cb8
@hexlet/cli <support@hexlet.io>
2023-10-02 18:45:09 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,33 +6,32 @@ import java.util.Map;
 // BEGIN
 public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
-        // Разделяем предложение на слова, используя пробел как разделитель
         String[] words = sentence.split(" ");
         Map<String, Integer> wordCount = new HashMap<>();
 
-        // Подсчитываем количество каждого слова
         for (String word : words) {
-            // Приводим слова к нижнему регистру, чтобы учесть регистронезависимость
-            String lowercaseWord = word.toLowerCase();
+            String lowercaseWord = word.toLowerCase().trim();
 
-            // Если слово уже есть в словаре, увеличиваем счетчик на 1, иначе добавляем новое слово
-            wordCount.put(lowercaseWord, wordCount.getOrDefault(lowercaseWord, 0) + 1);
+            if (!lowercaseWord.isEmpty()) {
+                wordCount.put(lowercaseWord, wordCount.getOrDefault(lowercaseWord, 0) + 1);
+            }
         }
-
         return wordCount;
     }
 
     public static String toString(Map<String, Integer> wordCount) {
         StringBuilder result = new StringBuilder();
-        result.append("{\n");
+        if (wordCount.isEmpty()){
+            return "{}";
+        }
+        result.append("{");
 
-        // Обходим все ключи (слова) в словаре
         for (String word : wordCount.keySet()) {
-            result.append("  ").append(word).append(": ").append(wordCount.get(word)).append("\n");
+            result.append("\n  ").append(word).append(": ").append(wordCount.get(word));
         }
 
-        result.append("}");
-
+        result.append("\n}");
+        System.out.println(result.toString());
         return result.toString();
     }
 }
