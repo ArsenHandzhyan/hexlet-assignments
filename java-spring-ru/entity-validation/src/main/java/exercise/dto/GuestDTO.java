61aac6191f346949a7a8f6ebb0da25cb3cb376d4
@hexlet/cli <support@hexlet.io>
2024-03-27 15:46:18 +0300

    submit java-spring-ru/entity-validation

---
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestDTO.java
@@ -0,0 +1,29 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class GuestDTO {
+    private long id;
+
+    @NotBlank(message = "Name must not be empty")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email must not be empty")
+    private String email;
+
+    @Pattern(regexp = "^\\+\\d{11,13}$", message = "Phone number must start with '+' and contain 11 to 13 digits")
+    private String phoneNumber;
+
+    @Pattern(regexp = "^\\d{4}$", message = "Club card must consist of exactly four digits")
+    private String clubCard;
+
+    @FutureOrPresent(message = "Club card valid until date must be in the present or future")
+    private LocalDate cardValidUntil;
+
+    private LocalDate createdAt;
+}
